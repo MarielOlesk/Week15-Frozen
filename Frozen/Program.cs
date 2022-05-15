ebc59e524cd5a724b0d95090f780dc3dedfc175e
Indrek Täht <indrek.taht@gmail.com>
2022-05-15 23:58:00 +0300

    new project

---
+++ Frozen/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Frozen
+{
+    class Program
+    {
+        class SecretSanta
+        {
+            string name;
+            string wish;
+
+            public SecretSanta(string _name, string _wish)
+            {
+                name = _name;
+                wish = _wish;
+            }
+
+            public string Name
+            {
+                get
+                {
+                    return name;
+                }
+            }
+            public string Wish
+            {
+                get
+                {
+                    return wish;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            List<SecretSanta> mySecretSanta = new List<SecretSanta>();
+            string[] childrenFromFile = GetDataFromFile();
+
+            foreach (string line in childrenFromFile)
+            {
+                string[] tempArray = line.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+
+                SecretSanta newSanta = new SecretSanta(tempArray[0], tempArray[1]);
+
+                mySecretSanta.Add(newSanta);
+            }
+            foreach (SecretSanta childrenFromList in mySecretSanta)
+            {
+                Console.WriteLine($"{childrenFromList.Name} wants {childrenFromList.Wish}");
+            }
+        }
+
+        public static string[] GetDataFromFile()
+        {
+            string filePath = @"D:\visual_studio_failid\samples\Frozen.txt";
+            string[] dataFromFile = File.ReadAllLines(filePath);
+
+            return dataFromFile;
+        }
+
+    }
+}
---
+++ OOPFiles/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace OOPAndFiles
+{
+
+    class Program
+    {
+        class Movie
+        {
+            string title;
+            string raiting;
+            string year;
+
+            public Movie(string _title, string _raiting, string _year)
+            {
+                title = _title;
+                raiting = _raiting;
+                year = _year;
+
+            }
+
+            public string Title
+            {
+                get
+                {
+                    return title;
+                }
+            }
+            public string Raiting
+            {
+                get
+                {
+                    return raiting;
+                }
+            }
+            public string Year
+            {
+                get
+                {
+                    return year;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            List<Movie> myMovies = new List<Movie>();
+            string[] moviesFromFile = GetDataFromFile();
+
+            foreach(string line in moviesFromFile)
+            {
+                string[] tempArray = line.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+
+                Movie newMovie = new Movie(tempArray[0], tempArray[1], tempArray[2]);
+
+                myMovies.Add(newMovie);
+            }
+            foreach(Movie moveFromList in myMovies)
+            {
+                Console.WriteLine($"Title --> {moveFromList.Title}. Rating --> {moveFromList.Raiting}. Year --> {moveFromList.Year}");
+            }
+
+        }
+
+        public static void DisplayElementsFromArray(string[] someArray)
+        {
+            foreach(string element in someArray)
+            {
+                Console.WriteLine($"String from array: {element}");
+            }
+        }
+
+
+        public static string[] GetDataFromFile()
+        {
+            string filePath = @"D:\visual_studio_failid\samples\fail movies.txt";
+            string[] dataFromFile = File.ReadAllLines(filePath);
+
+            return dataFromFile;
+        }
+    }
+}
